f7343e3f4c333f41adda5bb2a3077148825012b0
derrickdrye <dryd95@hotmail.com>
2023-07-06 14:22:10 -0500

    StaticExerciseComplete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write($"Fahrenheit -> Celsius: {TempConverter.FahrenheitToCelsius(32)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Celsius -> Fahrentheit:{TempConverter.CelsiusToFahrenheit(0)}");
+            Console.WriteLine();
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempInFahrenheit)
+        {
+            //C = (F - 32) * 5 / 9;
+            return (tempInFahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double tempInCelsius)
+        {
+            // F = (C * 9) / 5 + 32;
+            return (tempInCelsius * 9) / 5 + 32;
+        }
+    }
+}
